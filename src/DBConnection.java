c695bac740e68b71e177c6e670a76e1f41cdde9a
ranjithraj-001 <118262637+ranjithraj-001@users.noreply.github.com>
2025-11-29 17:15:50 +0530

    DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -2,10 +2,10 @@ import java.sql.*;
 
 
 public class DBConnection {
-// update these values to match your local Oracle DB
+
 public static final String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:xe";
-public static final String DB_USER = "system"; // change if you created a different user
-public static final String DB_PASS = "8725"; // change to your password
+public static final String DB_USER = "system";
+public static final String DB_PASS = "8725"; 
 
 
 static {
